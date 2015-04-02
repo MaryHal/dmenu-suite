eabb86b1f88f3955e4aaac8d4d14f176577a8485
MaryHal <maryhal@users.noreply.github.com>
2015-04-02 01:12:46 -0700

    Cleaner selection of backends, multi-word prompts work now.

--- lib/menu_helpers.sh
+++ lib/menu_helpers.sh
@@ -1,32 +1,58 @@
 #!/bin/bash
 
 MenuProg=""
+BACKEND=${1:-dmenu}
 
-# Do not use fzf by default
-USE_FZF=${1:-0}
-
-if [[ $USE_FZF == 1 ]]; then
-    MenuProg="fzf -x"
-else
-    # Common dmenu settings
-    source $HOME/bin/menu/lib/dmenurc
-    MenuProg="$DMENU -l 12"
-fi
+case "$BACKEND" in
+    'fzf')
+        MenuProg="fzf -x"
+        ;;
+    'dmenu')
+        # Dmenu 2: Open on laptop monitor.
+        source $HOME/bin/menu/lib/dmenurc
+        MenuProg="$DMENU -s 0"
+        ;;
+    'rofi')
+        MenuProg="rofi -dmenu"
+        ;;
+esac
 
 ###################
 ## Functions
 ###################
 
+function getPromptOption ()
+{
+    local prompt="$1"
+    local promptOption=""
+
+    [[ -z "$prompt" ]] && promptOption=""
+
+    case "$BACKEND" in
+        'fzf')
+            promptOption="--prompt"
+            ;;
+        'dmenu')
+            promptOption="-p"
+            ;;
+        'rofi')
+            promptOption="-p"
+            ;;
+    esac
+
+    echo "$promptOption"
+}
+
 # Feeding menu items and a prompt to Dmenu the regular way gets messy.
 # To save sanity, this function makes it as simple as:
 #   menu "Your Prompt" "Item A" ["Item B" "Item C" ...]
-menu ()
+function menu ()
 {
     # We grab the prompt message...
-    prompt="$1"
+    local prompt="$1"
     shift
 
-    items=""
+    local items=""
     # We will now iterate through the rest of the arguments...
     until [[ -z "$1" ]]; do
         # ...add the menu item to the list we're going to feed to Dmenu...
@@ -36,32 +62,23 @@ menu ()
         # ...and keep doing this until there are no more arguments.
     done
 
-    # Prompt "Adapter" for dmenu/fzf
-    MenuCmd="$MenuProg"
-    if [[ -n "$prompt" ]]; then
-        if [[ $USE_FZF == 1 ]]; then
-            MenuCmd="$MenuCmd --prompt $prompt"
-        else
-            # Dmenu prompt switch
-            MenuCmd="$MenuCmd -p $prompt"
-        fi
-    fi
+    # Prompt "Adapter" for our menuing backend
+    promptOption=$(getPromptOption "$prompt")
 
     # Now that we're done with that, we can feed the hungry Dmenu.
     # We feed the list though `head -c-1` first, to get rid of that
     # trailing newline, since Dmenu isn't smart enough to ignore it.
-    result=$(echo "$items" | head -c-1 | $MenuCmd)
-    echo $result
+    echo "$items" | head -c-1 | $MenuProg $promptOption "$prompt"
 }
 
 # We can use menu() function for yes/no prompts.
-confirm ()
+function confirm ()
 {
     menu "$*" 'No' 'Yes'
 }
 
 # And we can even use it for a simple notice.
-alert ()
+function alert ()
 {
     menu "$*" 'OK'
 }
