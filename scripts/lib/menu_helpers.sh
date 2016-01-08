b1470bc7f178d759857a7ebd26cf4e36f0e115c0
MaryHal <maryhal@users.noreply.github.com>
2016-01-07 20:25:10 -0800

    USE_VERTICAL option for dmenu

--- scripts/lib/menu_helpers.sh
+++ scripts/lib/menu_helpers.sh
@@ -3,6 +3,7 @@
 # Set to 1 is you want to use dmenurc instead of reading from the Xresources
 # database database (patched dmenu only (like dmenu2))
 USE_DMENURC=0
+USE_VERTICAL=0
 
 MenuProg=""
 promptOption=""
@@ -18,13 +19,17 @@ case "$BACKEND" in
             # $MENU_DIR should be set in parent script...
             source $MENU_DIR/lib/dmenurc
         else
-            DMENU="dmenu -i"
-            # DMENU="dmenu -i -l 12 -x 403 -y 200 -w 560"
+            if [[ $USE_VERTICAL == 1 ]]; then
+                DMENU="dmenu -i -l 12 -x 403 -y 200 -w 560"
+            else
+                DMENU="dmenu -i"
+            fi
         fi
 
         # Dmenu2 implements the '-s' option which allows us to choose which
         # monitor to open our menu on.
         MenuProg="$DMENU -s 0 "
+
         promptOption="-p"
         ;;
     'rofi')
