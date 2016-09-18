061d85e6f18ffd1a18fc2dc85f82ad2a01f7ff65
MaryHal <maryhal@users.noreply.github.com>
2016-09-17 20:46:13 -0700

    brightnessmenu.pl

---
+++ perl/brightnessmenu.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+use FindBin;
+use lib "$FindBin::Bin/";
+
+use MenuSuite;
+
+use Data::Dumper;
+$Data::Dumper::Sortkeys = 1;
+
+my @brightnessSteps = qw (100 80 60 40 20);
+my $brightness = MenuSuite::selectMenu("Brightness: ", \@brightnessSteps) || exit 0;
+
+system("xbacklight = $brightness");
