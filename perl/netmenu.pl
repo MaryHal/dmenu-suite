bf3c0a239be81a888ffe1e10b5fabc9ae8c2ed95
MaryHal <maryhal@users.noreply.github.com>
2016-10-13 19:31:43 -0700

    netmenu.pl added

---
+++ perl/netmenu.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+
+# This script assumes you can run "netctl" as a user through sudo without
+# entering a password. This can be done by editing /etc/sudoers via visudo.
+
+use warnings;
+use strict;
+
+use FindBin;
+use lib "$FindBin::Bin/";
+
+use MenuSuite;
+
+use Data::Dumper;
+$Data::Dumper::Sortkeys = 1;
+
+sub loadNetProfile
+{
+    my ($profileName) = @_;
+
+    system('sudo', 'netctl', 'switch-to', "$profileName");
+}
+
+my @rawList = `netctl list`;
+
+my %profileOptions;
+foreach my $profile (@rawList)
+{
+    chomp($profile);
+
+    $profileOptions{$profile} = sub
+    {
+        loadNetProfile($profile);
+    };
+}
+
+MenuSuite::runMenu("Profile: ", \%profileOptions) || exit 0;
