b221263a099a51690d61ba344bace19a862d4285
MaryHal <maryhal@users.noreply.github.com>
2016-12-05 20:37:28 -0800

    pwsafe.pl "Add" password menu option

--- perl/pwsafe.pl
+++ perl/pwsafe.pl
@@ -15,7 +15,7 @@ my $entriesFile = "/tmp/pwentries";
 
 sub DumpPasswordEntries
 {
-    my $ret = system("termite --class \"fzf-menu\" --geometry 480x80 -e \"pwsafe --list -o ${entriesFile}\"");
+    my $ret = system("termite --class \"fzf-menu\" --geometry 560x80 -e \"pwsafe --list -o ${entriesFile}\"");
 
     if ($ret != 0)
     {
@@ -36,21 +36,26 @@ sub ReadPasswordEntries
 sub GetUsernamePassword
 {
     my ($entry) = @_;
-    exec("termite --class \"fzf-menu\" --geometry 480x80 -e \"pwsafe -up ${entry}\"");
+    exec("termite --class \"fzf-menu\" --geometry 560x80 -e \"pwsafe -up ${entry}\"");
+}
+
+sub GetAddEntry
+{
+    exec("termite --class \"fzf-menu\" --geometry 560x80 -e \"pwsafe -add\"");
 }
 
 ## Build our menu
 
-my %entries;
+my %entries = (
+    '[Reload]' => \&DumpPasswordEntries,
+    '[Add]' => \&GetAddEntry
+    );
+
 foreach my $entry (ReadPasswordEntries())
 {
     $entries{$entry} = sub {
         GetUsernamePassword($entry);
     };
 }
-
-$entries{'[Reload]'} = sub {
-    DumpPasswordEntries();
-};
 
 MenuSuite::runMenu("Entry: ", \%entries) || exit 0;
